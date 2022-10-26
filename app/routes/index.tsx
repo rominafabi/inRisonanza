94f3d8877cb52fc28e1fe9d73b687eaca5af9fb2
Romina Fabi <rominafabi@air-di-romina.homenet.telecomitalia.it>
2022-10-26 14:03:27 +0200

    fix: fixing index page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,14 +1,38 @@
 import { Typography } from "@material-tailwind/react";
-/* import { useOptionalUser } from "~/utils"; */
+import { Link } from "@remix-run/react";
+import { useOptionalUser } from "~/utils";
 
 export default function Index() {
-/*   const user = useOptionalUser(); */
+  const user = useOptionalUser();
   return (
     <main className="min-h-screen min-w-screen">
       <section className="min-h-screen min-w-screen bg-cover bg-center bg-no-repeat bg-main-background">
         <div className="h-full w-full">
           <Typography></Typography>
         </div>
+        {user ? (
+                  <Link
+                    to="/notes"
+                    className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-blue-700 shadow-sm hover:bg-blue-50 sm:px-8"
+                  >
+                    View Notes for {user.email}
+                  </Link>
+                ) : (
+                  <div className="space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5 sm:space-y-0">
+                    <Link
+                      to="/join"
+                      className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-blue-700 shadow-sm hover:bg-blue-50 sm:px-8"
+                    >
+                      Sign up
+                    </Link>
+                    <Link
+                      to="/login"
+                      className="flex items-center justify-center rounded-md bg-blue-500 px-4 py-3 font-medium text-white hover:bg-blue-600"
+                    >
+                      Log In
+                    </Link>
+                  </div>
+                )}
       </section>
       <section className="bg-red-500 min-h-screen min-w-screen">
 
