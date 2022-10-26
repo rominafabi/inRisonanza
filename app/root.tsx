a3b16372c27a3963a6bed2f9ad236ec048392658
Romina Fabi <rominafabi@air-di-romina.homenet.telecomitalia.it>
2022-10-26 13:50:32 +0200

    feat: imported new favicon and deleted custom stuff

--- app/root.tsx
+++ app/root.tsx
@@ -18,7 +18,7 @@ export const links: LinksFunction = () => {
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "Remix Notes",
+  title: "Romina Fabi",
   viewport: "width=device-width,initial-scale=1",
 });
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./app/**/*.{ts,tsx,jsx,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'main-background': "url('../../public/images/background.jpeg')",
+      },
+    },
   },
   plugins: [],
 };
